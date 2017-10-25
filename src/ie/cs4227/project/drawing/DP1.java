bdcb075ce15dea6bae6854bd5995f3c5b762ca0d
lorraineuchiha <32896313+lorraineuchiha@users.noreply.github.com>
2017-10-25 18:42:40 +0100

    Add files via upload
    -Addition of Drawing package classes for Bridge pattern

---
+++ src/ie/cs4227/project/drawing/DP1.java
@@ -0,0 +1,16 @@
+package ie.cs4227.project.drawing;
+
+public class DP1 extends V1_Drawing {
+    public void draw_a_line()
+    {
+
+    }
+    public void draw_a_circle()
+    {
+
+    }
+    public void draw_text()
+    {
+
+    }
+}
